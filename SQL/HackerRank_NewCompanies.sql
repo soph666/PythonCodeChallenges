2677bbc2543398aa9ad4430629d0f85f20eded05
soph666 <51186320+soph666@users.noreply.github.com>
2022-03-10 21:18:10 -0800

    -- create examples for new companies and 15 days of sql

--- SQL/HackerRank_NewCompanies.sql
+++ SQL/HackerRank_NewCompanies.sql
@@ -1,2 +1,67 @@
 -- https://www.hackerrank.com/challenges/the-company/problem
 
+-- CREATE EXAMPLE
+CREATE TABLE Company
+  (
+     company_code VARCHAR(200), founder VARCHAR(200)
+  );
+
+INSERT INTO Company
+            (company_code, founder)
+VALUES      ("C1", "Monika"),
+            ("C2","Samantha");
+
+CREATE TABLE Lead_Manager
+  (
+     lead_manager_code VARCHAR(200), company_code VARCHAR(200)
+  );
+
+INSERT INTO Lead_Manager
+            (lead_manager_code, company_code)
+VALUES      ("LM1", "C1"),
+            ("LM2","C2");
+
+CREATE TABLE Senior_Manager
+  (
+     senior_manager_code VARCHAR(200),
+     lead_manager_code VARCHAR(200),
+     company_code VARCHAR(200)
+  );
+
+INSERT INTO Senior_Manager
+            (senior_manager_code, lead_manager_code, company_code)
+VALUES      ("SM1", "LM1", "C1"),
+            ("SM1", "LM1", "C1"),
+            ("SM3", "LM2","C2");
+
+CREATE TABLE Manager
+  (
+     manager_code VARCHAR(200),
+     senior_manager_code VARCHAR(200),
+     lead_manager_code VARCHAR(200),
+     company_code VARCHAR(200)
+  );
+
+INSERT INTO Manager
+            (manager_code, senior_manager_code, lead_manager_code, company_code)
+VALUES      ("M1", "SM1", "LM1", "C1"),
+            ("M2", "SM3", "LM2", "C2"),
+            ("M3", "SM3", "LM2", "C2");
+
+CREATE TABLE Employee
+  (
+     employee_code VARCHAR(200),
+     manager_code VARCHAR(200),
+     senior_manager_code VARCHAR(200),
+     lead_manager_code VARCHAR(200),
+     company_code VARCHAR(200)
+  );
+
+INSERT INTO Employee
+            (employee_code, manager_code, senior_manager_code, lead_manager_code, company_code)
+VALUES      ("E1", "M1", "SM1", "LM1", "C1"),
+            ("E2", "M1", "SM1", "LM1", "C1"),
+            ("E3", "M2", "SM3", "LM2", "C2"),
+            ("E4", "M3", "SM3", "LM2", "C2");
+
+
